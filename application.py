7a24c94601e47fb027363639adc8ea5659a96e4f
justinlikescows <120424747+justinlikescows@users.noreply.github.com>
2023-02-18 17:50:53 -0800

    Add files via upload
    added initial prototype code

---
+++ application.py
@@ -0,0 +1,227 @@
+import requests
+import base64
+import json
+import time
+import random
+import azure.cognitiveservices.speech as speechsdk
+import openai
+import os
+
+from flask import Flask, jsonify, render_template, request, make_response
+
+app = Flask(__name__)
+
+subscription_key = '34830f105e0341ae96cf5c8ceef6fb8e'
+region = "eastus"
+language = "en-US"
+voice = "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)"
+stext = ""
+userprofile = ['Pete', '20', '4']
+userprofile1 = ['Jack', '4', '4']
+userprofile2 = ['Oliver', '54', '4']
+i = 0
+
+@app.route("/")
+def index():
+    return render_template("signin.html")
+
+@app.route("/readalong")
+def readalong():
+    return render_template("readalong.html")
+
+@app.route("/gettoken", methods=["POST"])
+def gettoken():
+    fetch_token_url = 'https://%s.api.cognitive.microsoft.com/sts/v1.0/issueToken' %region
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key
+    }
+    response = requests.post(fetch_token_url, headers=headers)
+    access_token = response.text
+    return jsonify({"at":access_token})
+
+
+@app.route("/gettonguetwister", methods=["POST"])
+def gettonguetwister():
+    paragraph = gpt3(userprofile[0],userprofile[1],userprofile[2])
+    return jsonify({"tt":paragraph})
+
+@app.route("/getstory", methods=["POST"])
+def getstory():
+    id = int(request.form.get("id"))
+    stories = [["Read aloud the sentences on the screen.",
+        "We will follow along your speech and help you learn speak English.",
+        "Good luck for your reading lesson!"],
+        ["The Hare and the Tortoise",
+        "Once upon a time, a Hare was making fun of the Tortoise for being so slow.",
+        "\"Do you ever get anywhere?\" he asked with a mocking laugh.",
+        "\"Yes,\" replied the Tortoise, \"and I get there sooner than you think. Let us run a race.\"",
+        "The Hare was amused at the idea of running a race with the Tortoise, but agreed anyway.",
+        "So the Fox, who had consented to act as judge, marked the distance and started the runners off.",
+        "The Hare was soon far out of sight, and in his overconfidence,",
+        "he lay down beside the course to take a nap until the Tortoise should catch up.",
+        "Meanwhile, the Tortoise kept going slowly but steadily, and, after some time, passed the place where the Hare was sleeping.",
+        "The Hare slept on peacefully; and when at last he did wake up, the Tortoise was near the goal.",
+        "The Hare now ran his swiftest, but he could not overtake the Tortoise in time.",
+        "Slow and Steady wins the race."],
+        ["The Ant and The Dove",
+        "A Dove saw an Ant fall into a brook.",
+        "The Ant struggled in vain to reach the bank,",
+        "and in pity, the Dove dropped a blade of straw close beside it.",
+        "Clinging to the straw like a shipwrecked sailor, the Ant floated safely to shore.",
+        "Soon after, the Ant saw a man getting ready to kill the Dove with a stone.",
+        "Just as he cast the stone, the Ant stung the man in the heel, and he missed his aim,",
+        "The startled Dove flew to safety in a distant wood and lived to see another day.",
+        "A kindness is never wasted."]]
+    if(id >= len(stories)):
+        return jsonify({"code":201})
+    else:
+        return jsonify({"code":200,"storyid":id , "storynumelements":len(stories[id]),"story": stories[id]})
+
+@app.route("/ackaud", methods=["POST"])
+def ackaud():
+    f = request.files['audio_data']
+    reftext = request.form.get("reftext")
+    #    f.save(audio)
+    #print('file uploaded successfully')
+
+    # a generator which reads audio data chunk by chunk
+    # the audio_source can be any audio input stream which provides read() method, e.g. audio file, microphone, memory stream, etc.
+    def get_chunk(audio_source, chunk_size=1024):
+        while True:
+            #time.sleep(chunk_size / 32000) # to simulate human speaking rate
+            chunk = audio_source.read(chunk_size)
+            if not chunk:
+                #global uploadFinishTime
+                #uploadFinishTime = time.time()
+                break
+            yield chunk
+
+    # build pronunciation assessment parameters
+    referenceText = reftext
+    pronAssessmentParamsJson = "{\"ReferenceText\":\"%s\",\"GradingSystem\":\"HundredMark\",\"Dimension\":\"Comprehensive\",\"EnableMiscue\":\"True\"}" % referenceText
+    pronAssessmentParamsBase64 = base64.b64encode(bytes(pronAssessmentParamsJson, 'utf-8'))
+    pronAssessmentParams = str(pronAssessmentParamsBase64, "utf-8")
+
+    # build request
+    url = "https://%s.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=%s&usePipelineVersion=0" % (region, language)
+    headers = { 'Accept': 'application/json;text/xml',
+                'Connection': 'Keep-Alive',
+                'Content-Type': 'audio/wav; codecs=audio/pcm; samplerate=16000',
+                'Ocp-Apim-Subscription-Key': subscription_key,
+                'Pronunciation-Assessment': pronAssessmentParams,
+                'Transfer-Encoding': 'chunked',
+                'Expect': '100-continue' }
+
+    #audioFile = open('audio.wav', 'rb')
+    audioFile = f
+    # send request with chunked data
+    response = requests.post(url=url, data=get_chunk(audioFile), headers=headers)
+    #getResponseTime = time.time()
+    audioFile.close()
+
+    #latency = getResponseTime - uploadFinishTime
+    #print("Latency = %sms" % int(latency * 1000))
+
+    return response.json()
+
+@app.route("/gettts", methods=["POST"])
+def gettts():
+    reftext = request.form.get("reftext")
+    # Creates an instance of a speech config with specified subscription key and service region.
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
+    speech_config.speech_synthesis_voice_name = voice
+
+    offsets=[]
+
+    def wordbound(evt):
+        offsets.append( evt.audio_offset / 10000)
+
+    # Creates a speech synthesizer with a null output stream.
+    # This means the audio output data will not be written to any output channel.
+    # You can just get the audio from the result.
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+
+    # Subscribes to word boundary event
+    # The unit of evt.audio_offset is tick (1 tick = 100 nanoseconds), divide it by 10,000 to convert to milliseconds.
+    speech_synthesizer.synthesis_word_boundary.connect(wordbound)
+
+    result = speech_synthesizer.speak_text_async(reftext).get()
+    # Check result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        #print("Speech synthesized for text [{}]".format(reftext))
+        #print(offsets)
+        audio_data = result.audio_data
+        #print(audio_data)
+        #print("{} bytes of audio data received.".format(len(audio_data)))
+        
+        response = make_response(audio_data)
+        response.headers['Content-Type'] = 'audio/wav'
+        response.headers['Content-Disposition'] = 'attachment; filename=sound.wav'
+        # response.headers['reftext'] = reftext
+        response.headers['offsets'] = offsets
+        return response
+        
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+        return jsonify({"success":False})
+
+@app.route("/getttsforword", methods=["POST"])
+def getttsforword():
+    word = request.form.get("word")
+
+    # Creates an instance of a speech config with specified subscription key and service region.
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
+    speech_config.speech_synthesis_voice_name = voice
+
+    # Creates a speech synthesizer with a null output stream.
+    # This means the audio output data will not be written to any output channel.
+    # You can just get the audio from the result.
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+
+    result = speech_synthesizer.speak_text_async(word).get()
+    # Check result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        #print("Speech synthesized for text [{}]".format(reftext))
+        #print(offsets)
+        audio_data = result.audio_data
+        #print(audio_data)
+        #print("{} bytes of audio data received.".format(len(audio_data)))
+        
+        response = make_response(audio_data)
+        response.headers['Content-Type'] = 'audio/wav'
+        response.headers['Content-Disposition'] = 'attachment; filename=sound.wav'
+        # response.headers['word'] = word
+        return response
+        
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+        return jsonify({"success":False})
+
+def gpt3(name, age, skill_rating):
+    openai.api_key = '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'
+    stext = f"Write a reading comprehension paragraph for a {age} year old to practice reading. {name} has a reading skill level of {skill_rating} out of 10. Have 4 full sentences max. The story doesn't have to necessarily include them:"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=stext,
+        temperature=1.0,
+        max_tokens=120,
+        top_p=1
+    )
+    paragraph = response.choices[0].text
+    return paragraph.strip()
+
+def query():
+    name = input("What is your name?")
+    age = input("How old are you? ")
+    skill_rating = input("How would you rate your skill level(1-10)?")
+    return name, age, skill_rating
+
+
+# response = gpt3()
+# print(response)
---
+++ static/signin.js
@@ -0,0 +1,5 @@
+var sbutton = document.getElementsByTagName('sbutton')[0]
+
+sbutton.onclick = function() {
+    window.location.href = "index.html"; // navigate to the index.html file
+};
